ab20f01ad9911ab5b24a5dc3c70ae32e31a5cf33
Ravi Pal <ravi.pal@ucodesoft.com>
2022-12-09 16:10:17 +0530

    created productdetails page

--- src/components/client/ProductDetails.client.jsx
+++ src/components/client/ProductDetails.client.jsx
@@ -7,30 +7,37 @@ import {
   AddToCartButton,
 } from "@shopify/hydrogen";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function ProductDetails({ product }) {
   return (
-    <ProductOptionsProvider data={product}>
-      <section className="w-full overflow-x-hidden gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-        <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
-          <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2">
-            <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
-              <ProductGallery media={product.media.nodes} />
-            </div>
+    <section className="mt-2">
+      <ProductOptionsProvider data={product}>
+        <div className="flex justify-between flex-wrap mx-auto lg:w-10/12">
+          <div className="lg:w-2/5 mx-auto">
+            <ProductGallery media={product.media.nodes} />
           </div>
-          <div className="sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-8 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
-            <div className="grid gap-2">
-              <h1 className="text-4xl font-bold leading-10 whitespace-normal">
-                {product.title}
-              </h1>
-              <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
-                {product.vendor}
-              </span>
+          <div className="lg:w-3/5 mt-8 lg:mt-0">
+            <div className="lg:w-4/5 lg:mx-auto mt-4 px-2">
+              <p className="text-3xl font-bold">{product.title}</p>
+              <p className="text-gray-500 mb-4">{product.vendor}</p>
+              <div className="lg:w-3/5">
+                <ProductForm product={product} />
+              </div>
+              <div className="mt-8">
+                <h3 className="text-xl font-bold">Description</h3>
+                <div
+                  className="prose border-t border-gray-200 pt-6 text-black text-md"
+                  dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+                ></div>
+              </div>
             </div>
-            <ProductForm product={product} />
           </div>
         </div>
-      </section>
-    </ProductOptionsProvider>
+      </ProductOptionsProvider>
+    </section>
   );
 }
 
@@ -97,12 +104,12 @@ function PurchaseMarkup() {
         </span>
       </AddToCartButton>
       {isOutOfStock ? (
-        <span className="text-black text-center py-3 px-6 border rounded-sm leading-none ">
+        <span className="text-black text-center py-3 px-6 border rounded leading-none ">
           Available in 2-3 weeks
         </span>
       ) : (
         <BuyNowButton variantId={selectedVariant.id}>
-          <span className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none border w-full">
+          <span className=" bg-violet-600 text-white inline-block rounded font-medium text-center py-3 px-6 max-w-xl leading-none border w-full">
             Buy it now
           </span>
         </BuyNowButton>
@@ -118,7 +125,6 @@ function OptionRadio({ values, name }) {
       {values.map((value) => {
         const checked = selectedOptions[name] === value;
         const id = `option-${name}-${value}`;
-
         return (
           <label key={id} htmlFor={id}>
             <input
@@ -148,42 +154,49 @@ function ProductGallery({ media }) {
   if (!media.length) {
     return null;
   }
-  return (
-    <>
-    <div className="border border-red-500 w-2/3">
+  const settings = {
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: false,
+    arrows: false
+  };
+  return(
+    <div>
+    <Slider {...settings} className = "lg:w-full  mx-auto lg:h-full h-[500px] w-[400px]">
       {
-        media?.map((med, i) => {
-          let extraProps = {}
-          if(med.mediaContentType === 'MODEL_3D') {
+        media.map((med, i) => {
+          let extraProps = {};
+          if (med.mediaContentType === "MODEL_3D") {
             extraProps = {
-              interactionPromptThreshold: '0',
+              interactionPromptThreshold: "0",
               ar: true,
               loading: "eager",
-              disableZoom:true
-            }
+              disableZoom: true,
+            };
           }
           const data = {
-            mediaContentType: med.mediaContentType,
+            ...med,
             image: {
               ...med.image,
-              altText: med.alt || "Product image"
-            }
-          }
+              altText: med.alt || "Product image",
+            },
+          };  
           return (
-            <div 
-              key={med.id || i}
-              className="w-2/3"
-            >
+            <div key={i} className = "lg:w-[400px] lg:h-[550px]">
               <MediaFile
-                tabIndex = "0"
                 data={data}
+                className = "w-full h-full object-contain"
                 {...extraProps}
               />
             </div>
           )
-        })
+        }
+      )
       }
+      </Slider>
     </div>
-    </>
   )
 }
